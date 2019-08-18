7290479e22d9a99542f4002c78317b2ae337d161
DreamzRunzLA <salmaan.a.qadir@gmail.com>
2019-08-18 17:08:41 -0600

    Finished recape-exercise-2

--- lib/problems.rb
+++ lib/problems.rb
@@ -1,21 +1,42 @@
 # Write a method, least_common_multiple, that takes in two numbers and returns the smallest number that is a mutiple 
 # of both of the given numbers
 def least_common_multiple(num_1, num_2)
-
+    upper_bounds = num_1 > num_2 ? num_1 : num_2
+    solved = false
+    while solved == false do 
+        if upper_bounds % num_1 == 0 && upper_bounds % num_2 == 0
+            solved = true
+            upper_bounds -= 1
+        end
+        upper_bounds += 1
+    end
+    return upper_bounds
 end
 
 
 # Write a method, most_frequent_bigram, that takes in a string and returns the two adjacent letters that appear the
 # most in the string.
 def most_frequent_bigram(str)
-
+    check = Hash.new(0)
+    str.each_char.with_index do |char1, i|
+        str.each_char.with_index do |char2, k|
+            if k > i && (k-i == 1)
+                check[char1 + char2] += 1
+            end
+        end
+    end
+    return check.key(check.values.max)
 end
 
 
 class Hash
     # Write a method, Hash#inverse, that returns a new hash where the key-value pairs are swapped
     def inverse
-
+        newHash = Hash.new
+        self.keys.each do |key|
+            newHash[self[key]] = key
+        end
+        return newHash
     end
 end
 
@@ -23,7 +44,15 @@ end
 class Array
     # Write a method, Array#pair_sum_count, that takes in a target number returns the number of pairs of elements that sum to the given target
     def pair_sum_count(num)
-
+        count = 0
+        self.each_with_index do |ele1, i|
+            self.each_with_index do |ele2, k|
+                if k > i && (ele1 + ele2 == num)
+                    count += 1
+                end
+            end
+        end
+        return count
     end
 
 
@@ -31,6 +60,20 @@ class Array
     # When given a proc, the method should sort the array according to the proc.
     # When no proc is given, the method should sort the array in increasing order.
     def bubble_sort(&prc)
+        prc ||= Proc.new { |i, k| i <=> k } 
+        
+        sorted = false
 
+        while !sorted
+            sorted = true
+            (0...self.length - 1).each do |i|
+                if prc.call(self[i], self[i+1]) == 1
+                    sorted = false
+                    self[i], self[i+1] = self[i+1], self[i]
+                end
+            end
+        end
+
+        return self
     end
 end
